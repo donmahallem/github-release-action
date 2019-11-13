559e761860f7e85fdbc47db95eca8f3a9c6af587
donmahallem <donmahallem@users.noreply.github.com>
2019-11-13 12:02:02 +0100

    fix test

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+/*!
+ * Source https://github.com/donmahallem/FlowApiTypes
+ */
+
+import { expect } from "chai";
+import "mocha";
+import * as index from "./index";
+
+describe("index", () => {
+    it("should contain FlowApiValidator");
+});
